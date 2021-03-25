1bd8be80c6c8347ae5af83d5eb3526775c03087a
Terry <terrykuo501@gmail.com>
2021-03-25 17:11:31 +0800

    Initial Commit

--- CustumTabBar/ContentView.swift
+++ CustumTabBar/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let sf = ["pencil.circle","square.and.arrow.down.on.square","plus.app.fill","trash","person.badge.plus"]
+    @State var isSelected = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            switch isSelected {
+            case 0:
+                NavigationView {
+                    Text("Text")
+                        .navigationTitle("First")
+                }
+            default:
+                Text("Text Other")
+            }
+            
+            
+            Spacer()
+            Divider()
+            HStack {
+                ForEach(0..<sf.count) { index in
+                    Button(action: {
+                        isSelected = index
+                    }, label: {
+                        Spacer()
+                        Image(systemName: sf[index])
+                            .font(.system(size: 25))
+                            .foregroundColor(.black)
+                        Spacer()
+                    })
+                    
+                    
+                }
+            }
+        }
     }
 }
 

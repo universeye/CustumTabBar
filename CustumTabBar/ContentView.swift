5cd209b0aaab4c789040b197a42d09dea0f16aae
Terry <terrykuo501@gmail.com>
2021-03-25 16:58:23 +0800

    Initial Commit

---
+++ CustumTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustumTabBar
+//
+//  Created by Terry Kuo on 2021/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

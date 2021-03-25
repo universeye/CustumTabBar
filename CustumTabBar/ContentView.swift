8f0da772d0f4f9a002d33ecae9ba52dfd5bf4af0
Terry <terrykuo501@gmail.com>
2021-03-25 21:28:27 +0800

    Add .fullScreenCover, add Model

--- CustumTabBar/ContentView.swift
+++ CustumTabBar/ContentView.swift
@@ -11,46 +11,66 @@ struct ContentView: View {
     
     let sf = ["pencil.circle","square.and.arrow.down.on.square","plus.app.fill","trash","person.badge.plus"]
     @State var isSelected = 0
+    @State var shouldShowModel = false
     
     var body: some View {
-        VStack {
-            
-            switch isSelected {
-            case 0:
-                NavigationView {
-                    Text("Text")
-                        .navigationTitle("First")
-                }
-            default:
-                Text("Text Other")
-            }
-            
+        ZStack {
             
             Spacer()
-            Divider()
-            HStack {
-                ForEach(0..<sf.count) { index in
+                .fullScreenCover(isPresented: $shouldShowModel, content: {
                     Button(action: {
-                        isSelected = index
+                        shouldShowModel = false
                     }, label: {
-                        Spacer()
-                        if index == 2 {
-                            Image(systemName: sf[index])
-                                .font(.system(size: 35))
-                                .foregroundColor(.red)
-                        } else if isSelected == index {
-                            Image(systemName: sf[index])
-                                .font(.system(size: 25))
-                                .foregroundColor(.black)
-                        } else {
-                            Image(systemName: sf[index])
-                                .font(.system(size: 25))
-                                .foregroundColor(.gray)
-                        }
-                        Spacer()
+                        Text("Model Text")
                     })
                     
-                    
+                })
+            
+            VStack {
+                
+                switch isSelected {
+                case 0:
+                    NavigationView {
+                        Text("Text")
+                            .navigationTitle("First")
+                    }
+                default:
+                    Text("Text Other")
+                }
+                
+                
+                Spacer()
+                Divider()
+                HStack {
+                    ForEach(0..<sf.count) { index in
+                        Button(action: {
+                            
+                            if index == 2 {
+                                shouldShowModel.toggle()
+                            } else {
+                                isSelected = index
+                            }
+                            
+                        }, label: {
+                            Spacer()
+                            if index == 2 {
+                                Image(systemName: sf[index])
+                                    .font(.system(size: 35))
+                                    .foregroundColor(.red)
+                            } else if isSelected == index {
+                                Image(systemName: sf[index])
+                                    .font(.system(size: 25))
+                                    .foregroundColor(.black)
+                            } else {
+                                Image(systemName: sf[index])
+                                    .font(.system(size: 25))
+                                    .foregroundColor(.gray)
+                            }
+                            Spacer()
+                        })
+                        
+                        
+                    }
                 }
             }
         }
